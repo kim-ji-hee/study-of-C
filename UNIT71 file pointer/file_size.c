00b3d64efd02eff36989334c346bea7638d6df4a
kim-ji-hee <dream_821@naver.com>
2020-01-28 11:48:12 +0900

    UNIT71 file pointer

---
+++ UNIT71 file pointer/file_size.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+    int size;
+
+    FILE *fp = fopen("hello.txt", "r");
+
+    fseek(fp, 0, SEEK_END);
+    size = ftell(fp);
+
+    printf("%d\n", size);
+
+    fclose(fp);
+
+    return 0;
+}
