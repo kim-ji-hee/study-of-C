9905ddf82a3de3884d4145ce913e1db6f0d84128
kim-ji-hee <dream_821@naver.com>
2020-03-06 14:10:53 +0900

    UNIT82 filearchive

--- UNIT82 filearchive/test.c
+++ UNIT82 filearchive/test.c
@@ -120,13 +120,13 @@ int append(PARCHIVE archive, char *filename)    //파일 추가 함수 정의
 
 void list(PARCHIVE archive)    // 파일 목록 출력 함수 정의
 {
-    printf("파일 목록:\n");
+    //printf("파일 목록:\n");
 
     // 파일 목록 연결 리스트를 순회하면서 파일 이름 출력
     PFILE_NODE curr = archive->fileList.next;    // 첫 번째 노드
     while (curr != NULL)
     {
-        printf("    %s\n", curr->desc.name);
+        printf("%s\n", curr->desc.name);
 
         curr = curr->next;
     }
@@ -195,32 +195,8 @@ int main()
 
     PFILE_NODE curr;
 
-    FILE *fp = fopen(ARCHIVE_NAME, "r+b");  //아카이브 파일을 읽기/쓰기 모드로 열기
-    if (fp == NULL)                         //아카이브 파일이 없으면
-    {
-        fp = fopen(ARCHIVE_NAME, "w+b");    //아카이브 파일을 생성
-        if (fp == NULL)                     //아카이브 생성(열기)에 실패하면
-            return -1;                      //프로그램 종료
-
-        //새 아카이브 헤더 생성
-        archive->header.magic = 'AF';   //매직 넘버 AF 저장 (리틀엔디언에서는 FA로 저장됨)
-        archive->header.version = 1;    //파일 버전 1 저장
-
-        //아카이브 파일에 아카이브 헤더 저장
-        if(fwrite(&archive->header, sizeof(ARCHIVE_HEADER), 1, fp) < 1)
-        {
-            printf("아카이브 헤더 쓰기 실패\n");
-            fclose(fp);
-            return -1;
-        }
-
-        archive->fp = fp;   //아카이브 파일 포인터 저장
-
-        append(archive, "hello.txt");    //hello.txt 파일 추가
-    }
-
-    else //아카이브 파일이 있으면
-    {
+    FILE *fp = fopen(ARCHIVE_NAME, "rb");  //아카이브 파일을 읽기/쓰기 모드로 열기
+    
         //아카이브 파일에서 아카이브 헤더 읽기
         if (fread(&archive->header, sizeof(ARCHIVE_HEADER), 1, fp) < 1)
         {
@@ -228,10 +204,10 @@ int main()
             fclose(fp);
             return -1;
         }
-    }
+    
 
     //아카이브 파일 매직 넘버 검사
-    if (archive->header.magic != 'AF')
+    if (archive->header.magic != 'XR')
     {
         printf("아카이브 파일이 아닙니다.\n");
         fclose(fp);
@@ -239,7 +215,7 @@ int main()
     }
 
     //아카이브 파일 버전 검사
-    if (archive->header.version != 1)
+    if (archive->header.version != 2)
     {
         printf("버전이 맞지 않습니다.\n");
         fclose(fp);
@@ -275,8 +251,8 @@ int main()
         fseek(fp, currPos, SEEK_SET);
     }
 
-    //list(archive);  //파일 목록 출력
-    extract(archive, "hello.txt");  //hello.txt 파일 추출
+    list(archive);  //파일 목록 출력
+    //extract(archive, "hello.txt");  //hello.txt 파일 추출
 
 FINALIZE :  //파일 목록 연결 리스트를 순회하면서 메모리 해제ㅔ
     curr = archive->fileList.next;  //첫 번째 노드
